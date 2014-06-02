42f0e9400d676abe12e0aa63e643158104f5da30
Michel Valin <mfvalin@gmail.com>
2014-06-02 20:05:50 +0000

    some .f became .f90, fixed comments

--- rpncomm/RPN_COMM_barrier.f90
+++ rpncomm/RPN_COMM_barrier.f90
@@ -1,22 +1,22 @@
-*/* RMNLIB - Library of useful routines for C and FORTRAN programming
-* * Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
-* *                          Environnement Canada
-* *
-* * This library is free software; you can redistribute it and/or
-* * modify it under the terms of the GNU Lesser General Public
-* * License as published by the Free Software Foundation,
-* * version 2.1 of the License.
-* *
-* * This library is distributed in the hope that it will be useful,
-* * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* * Lesser General Public License for more details.
-* *
-* * You should have received a copy of the GNU Lesser General Public
-* * License along with this library; if not, write to the
-* * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-* * Boston, MA 02111-1307, USA.
-* */
+!/* RMNLIB - Library of useful routines for C and FORTRAN programming
+! * Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
+! *                          Environnement Canada
+! *
+! * This library is free software; you can redistribute it and/or
+! * modify it under the terms of the GNU Lesser General Public
+! * License as published by the Free Software Foundation,
+! * version 2.1 of the License.
+! *
+! * This library is distributed in the hope that it will be useful,
+! * but WITHOUT ANY WARRANTY; without even the implied warranty of
+! * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! * Lesser General Public License for more details.
+! *
+! * You should have received a copy of the GNU Lesser General Public
+! * License along with this library; if not, write to the
+! * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+! * Boston, MA 02111-1307, USA.
+! */
         module RPN_COMM_barrier_priv
           integer, save :: detail = 0   ! 0, 1, 2
           integer, save :: isize = -1   ! valid number of data points in times array
@@ -28,13 +28,13 @@
 
         SUBROUTINE RPN_COMM_barrier(com,ierr)
 
-c	Luc Corbeil, 2000-11-21
-c	mpi barrier
+!	Luc Corbeil, 2000-11-21
+!	mpi barrier
         use RPN_COMM_barrier_priv
         implicit none
         integer :: comm,ierr
         character(len=*) com
-**
+!*
         include 'mpif.h'
 
 	integer RPN_COMM_comm
