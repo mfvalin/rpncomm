621deb9fbe258c4e1e86554a05d123adb732fbb4
Michel <armnmfv@nemesis.(none)>
2014-06-20 13:15:24 -0400

    f90 ification

--- rpncomm/RPN_COMM_openmp.f90
+++ rpncomm/RPN_COMM_openmp.f90
@@ -7,8 +7,8 @@
 !
 !	no underscore version  (C style)
 !
-	function f_omp_get_max_threads() result(r)
-     %                 bind(C,name='f_omp_get_max_threads')
+	function f_omp_get_max_threads() result(r)&
+     &                 bind(C,name='f_omp_get_max_threads')
 	use iso_c_binding
 	implicit none
         integer(C_INT) :: r
@@ -18,8 +18,8 @@
 	r=omp_get_max_threads()
 	end function f_omp_get_max_threads
 !
-	subroutine f_omp_set_num_threads(nthreads)
-     %                 bind(C,name='f_omp_set_num_threads')
+	subroutine f_omp_set_num_threads(nthreads)&
+     &                 bind(C,name='f_omp_set_num_threads')
 	use iso_c_binding
 	implicit none
 	integer(C_INT), intent(IN) :: nthreads
@@ -29,8 +29,8 @@
 !
 !	one underscore version
 !
-	function f_omp_get_max_threads_() result(r)
-     %                 bind(C,name='f_omp_get_max_threads_')
+	function f_omp_get_max_threads_() result(r)&
+     &                 bind(C,name='f_omp_get_max_threads_')
 	use iso_c_binding
 	implicit none
         integer(C_INT) :: r
@@ -40,8 +40,8 @@
 	r=omp_get_max_threads()
 	end function f_omp_get_max_threads_
 !
-	subroutine f_omp_set_num_threads_(nthreads)
-     %                 bind(C,name='f_omp_set_num_threads_')
+	subroutine f_omp_set_num_threads_(nthreads)&
+     &                 bind(C,name='f_omp_set_num_threads_')
 	use iso_c_binding
 	implicit none
 	integer(C_INT), intent(IN) :: nthreads
@@ -51,8 +51,8 @@
 !
 !	two underscores version
 !
-	function f_omp_get_max_threads__() result(r)
-     %                 bind(C,name='f_omp_get_max_threads__')
+	function f_omp_get_max_threads__() result(r)&
+     &                 bind(C,name='f_omp_get_max_threads__')
 	use iso_c_binding
 	implicit none
         integer(C_INT) :: r
@@ -62,8 +62,8 @@
 	r=omp_get_max_threads()
 	end function f_omp_get_max_threads__
 !
-	subroutine f_omp_set_num_threads__(nthreads)
-     %                 bind(C,name='f_omp_set_num_threads__')
+	subroutine f_omp_set_num_threads__(nthreads)&
+     &                 bind(C,name='f_omp_set_num_threads__')
 	use iso_c_binding
 	implicit none
 	integer(C_INT), intent(IN) :: nthreads
--- rpncomm/RPN_COMM_oper.f90
+++ rpncomm/RPN_COMM_oper.f90
@@ -1,31 +1,31 @@
-*/* RMNLIB - Library of useful routines for C and FORTRAN programming
-* * Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
-* *                          Environnement Canada
-* *
-* * This library is free software; you can redistribute it and/or
-* * modify it under the terms of the GNU Lesser General Public
-* * License as published by the Free Software Foundation,
-* * version 2.1 of the License.
-* *
-* * This library is distributed in the hope that it will be useful,
-* * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* * Lesser General Public License for more details.
-* *
-* * You should have received a copy of the GNU Lesser General Public
-* * License along with this library; if not, write to the
-* * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-* * Boston, MA 02111-1307, USA.
-* */
-
-        integer function RPN_COMM_oper(op)
+!/! RMNLIB - Library of useful routines for C and FORTRAN programming
+! ! Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
+! !                          Environnement Canada
+! !
+! ! This library is free software; you can redistribute it and/or
+! ! modify it under the terms of the GNU Lesser General Public
+! ! License as published by the Free Software Foundation,
+! ! version 2.1 of the License.
+! !
+! ! This library is distributed in the hope that it will be useful,
+! ! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! ! Lesser General Public License for more details.
+! !
+! ! You should have received a copy of the GNU Lesser General Public
+! ! License along with this library; if not, write to the
+! ! Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+! ! Boston, MA 02111-1307, USA.
+! */
+!InTf!
+        integer function RPN_COMM_oper(op)              !InTf!
 	use rpn_comm
-c	Luc Corbeil, 2000-11-20
-c	lien entre datatype et MPI_datatype
+!	Luc Corbeil, 2000-11-20
+!	lien entre datatype et MPI_datatype
 
-        implicit none
+        implicit none                                   !InTf!
 !        include 'mpif.h'
-        character(len=*) op
+        character(len=*), intent(IN) :: op              !InTf!
         character(len=32) operation
         integer :: i
 
@@ -100,4 +100,4 @@ c	lien entre datatype et MPI_datatype
         stop
           
         return
-        end
+        end function RPN_COMM_oper                  !InTf!
