38a55843233bc1c88fc9b98fa54b5b8bf43e54ed
Michel Valin <mfvalin@gmail.com>
2014-08-21 11:26:52 +0000

    documantation

--- rpncomm/RPN_COMM_haloflip.f90
+++ rpncomm/RPN_COMM_haloflip.f90
@@ -1,11 +1,37 @@
-subroutine RPN_COMM_haloflip(g,minx,maxx,miny,maxy,ni,nj,nk,halox,haloy,gni)
-  use rpn_comm
-  implicit none
+!/* RMNLIB - Library of useful routines for C and FORTRAN programming
+! * Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
+! *                          Environnement Canada
+! *
+! * This library is free software; you can redistribute it and/or
+! * modify it under the terms of the GNU Lesser General Public
+! * License as published by the Free Software Foundation,
+! * version 2.1 of the License.
+! *
+! * This library is distributed in the hope that it will be useful,
+! * but WITHOUT ANY WARRANTY; without even the implied warranty of
+! * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! * Lesser General Public License for more details.
+! *
+! * You should have received a copy of the GNU Lesser General Public
+! * License along with this library; if not, write to the
+! * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+! * Boston, MA 02111-1307, USA.
+! */
+!InTf!
+!**S/R RPN_COMM_haloflip handle across the pole halo exchange for global grids
+subroutine RPN_COMM_haloflip(g,minx,maxx,miny,maxy,ni,nj,nk,halox,haloy,gni)   !InTf!
+  use rpn_comm                                                                 !InTf!
+  implicit none                                                                !InTf!
   include 'RPN_COMM_interfaces.inc'
-  integer, intent(IN) :: minx,maxx,miny,maxy,ni,nj,nk,halox,haloy,gni
-  integer, intent(INOUT) :: g(minx:maxx,miny:maxy,nk)
-
-  integer :: i,j,k,ierr,gmin,gmax,i0,in
+!ARGUMENTS
+  integer, intent(IN) :: minx,maxx,miny,maxy,ni,nj,nk,halox,haloy,gni          !InTf!
+  integer, intent(INOUT) :: g(minx:maxx,miny:maxy,nk)                          !InTf!
+! minx,maxx,miny,maxy,nk     dimensions of local portion of the global grid
+! ni,nj                      number of useful points along x and y in local portion of grid
+! halox,haloy                halo sizes
+! gni                        dimension along x of the global grid
+  integer :: i,j,k,ierr,gmin,gmax
+!  integer :: i0,in
   integer :: j_src,j_dest
   integer, dimension(minx:maxx,haloy,nk,0:1) :: local
   integer, dimension(0:pe_nx) :: count, depl
@@ -14,62 +40,59 @@ subroutine RPN_COMM_haloflip(g,minx,maxx,miny,maxy,ni,nj,nk,halox,haloy,gni)
   integer :: ipe
   integer :: gis, gie, l_offset, g_offset
   integer, dimension(MPI_STATUS_SIZE) :: status
+!NOTES
+! each PE starts by collecting its polar halo area
+! this area is then circulated ring fashion in the x direction between PEs
+! each PE will pick on the fly from this circulated data what it needs for its transpolar area
 
-  if(.not. bnd_south .and. .not. bnd_north) return
-  if(bnd_south) then
-    j_src = 1
-    j_dest = 1-haloy
+  if(.not. bnd_south .and. .not. bnd_north) return ! nothing to do if not containing north or south pole
+  if(bnd_south) then                               ! south pole
+    j_src = 1             ! first source row
+    j_dest = 1-haloy      ! first destination row
   endif
-  if(bnd_north) then
+  if(bnd_north) then                               ! north pole
     j_src = nj - haloy + 1
     j_dest = nj + 1
   endif
-  ierr = RPN_COMM_limit(pe_mex,pe_nx,1,gni,gmin,gmax,count,depl)
-  do k=1,nk
-  do j=1,haloy
-  do i=minx,maxx
-    local(i,j,k,0) = g(i,j_src+haloy-j,k)
-  enddo
-  enddo
-  enddo
-!  do k=nk,1,-1
-!  do j=haloy,1,-1
-!    print 1,local(:,j,k,0)
-!  enddo
-!  enddo
-!  print *,"-------------------------------------------------------"
+  ierr = RPN_COMM_limit(pe_mex,pe_nx,1,gni,gmin,gmax,count,depl)  ! get distribution along x
 
-  start = gmin + minx -1 + gni/2
-  finish = start + (maxx - minx)
-!  print *,'start=',start,' finish=',finish
-  if(start > gni) then
-    start = start - gni
+  start = gmin + minx -1 + gni/2  ! index of first point in global space of region across pole
+  finish = start + (maxx - minx)  ! index of last point in global space of region across pole
+  if(start > gni) then            ! the whole region is wrapped around end of grid
+    start = start - gni           ! wrap modulo gni
     finish = finish - gni
   endif
-  if(finish > gni) then
+  if(finish > gni) then           ! 2 pieces, start thru gni + 1 thru finish2
     start2 = 1
     finish2 = finish - gni
     finish = gni
-  else
+  else                            ! one piece only 
     start2 = 0
     finish2 = 0
   endif
-  send = 0;
-  recv = 1;
-  ihave = pe_mex
-  nwds = nk * haloy * (maxx-minx+1)
+!
+  send = 0;                       ! this PE will send local(:,:,:,send)
+  recv = 1;                       ! this PE will receive local(:,:,:,recv)
+  ihave = pe_mex                  ! incoming buffer contains halo along y of tile "ihave" in local(:,:,:,send)
+  do k=1,nk
+  do j=1,haloy
+  do i=minx,maxx
+    local(i,j,k,0) = g(i,j_src+haloy-j,k)   ! get halo along y, reverse rows along y (j)
+  enddo
+  enddo
+  enddo
+  nwds = nk * haloy * (maxx-minx+1)  ! number of elements of said halo
   g_offset = minx - start
-!  print *,'start=',start,' finish=',finish
-!  print *,'start2=',start2,' finish2=',finish2
+!
   do ipe = 1, pe_nx
-    low = depl(ihave) + 1
+    low = depl(ihave) + 1            ! the incoming data goes from low to high in global index space
     high = low + count(ihave) -1
     l_offset = 1 - low
-    gis = max(low,start)
+    gis = max(low,start)             ! this PE looks for start thru finish in global index space
     gie = min(high,finish)
-    print *,'low=',low,' high=',high
-    if (gis <= gie) then
-      print *,'%gis=',gis,gis+g_offset,' gie=',gie,gie+g_offset
+!    print *,'low=',low,' high=',high
+    if (gis <= gie) then             ! there is an overlap, collect points for gis thru gie in global index space
+!      print *,'%gis=',gis,gis+g_offset,' gie=',gie,gie+g_offset
       do k=1,nk
       do j=1,haloy
       do i=gis,gie
@@ -78,7 +101,7 @@ subroutine RPN_COMM_haloflip(g,minx,maxx,miny,maxy,ni,nj,nk,halox,haloy,gni)
       enddo
       enddo
     endif
-    if(low <= finish2) then
+    if(low <= finish2) then          ! there is an overlap for piece no 2, collect points for gis thru gie
 !      print *,'low=',low,' finish2=',finish2,' high=',high
       gis = low
       gie = min(high,finish2)
@@ -100,14 +123,14 @@ subroutine RPN_COMM_haloflip(g,minx,maxx,miny,maxy,ni,nj,nk,halox,haloy,gni)
     call mpi_sendrecv(local(minx,1,1,send) , nwds, MPI_INTEGER, send_to,   send_tag,   &
                       local(minx,1,1,recv) , nwds, MPI_INTEGER, recv_from, recv_tag,   &
                       pe_defcomm,status,ierr)
-    send = 1 - send
+    send = 1 - send   ! swap send and receive buffers
     recv = 1 - recv
-    ihave = ihave - 1
-    if(ihave<0) ihave = ihave + pe_nx
+    ihave = ihave - 1                    ! update ihave by shifting on poition towards west
+    if(ihave<0) ihave = ihave + pe_nx    ! wrap around
   enddo
 
-  i0 = 1
-  in = count(pe_mex)
+!  i0 = 1
+!  in = count(pe_mex)
 !  do k=1,nk
 !  do j=1,haloy
 !  do i=i0,in
@@ -117,8 +140,8 @@ subroutine RPN_COMM_haloflip(g,minx,maxx,miny,maxy,ni,nj,nk,halox,haloy,gni)
 !  enddo
 1 format(20I7.6)
   return
-end subroutine RPN_COMM_haloflip
-subroutine RPN_COMM_haloflip_test
+end subroutine RPN_COMM_haloflip                           !InTf!
+subroutine RPN_COMM_haloflip_test   ! basic test for RPN_COMM_haloflip
   use rpn_comm
   implicit none
   include 'RPN_COMM_interfaces.inc'
