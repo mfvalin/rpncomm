77152735bde1522cb90faa610f143849d8e4e2f2
Michel Valin <mfvalin@gmail.com>
2015-02-26 18:57:51 +0000

    small corrections + documentation

--- rpncomm/RPN_COMM_io_dist_coll.F90
+++ rpncomm/RPN_COMM_io_dist_coll.F90
@@ -33,6 +33,17 @@
 !
 ! start_x and start_y are in origin(1)
 !
+! important notes:
+!    the following parameters MUST de the same on ALL PEs
+!      setno
+!      mini,maxi,minj,maxj,lnk  (dimensions of the "local" array)
+!      gni,gnj,gnk   (dimensions of the "global" array)
+!      nx,ny,start_x,count_x,start_y,count_y
+!      periodx,periody
+!
+!    even if not used/necessary on a given PE
+!      the "global" array must exist ( (1,1,1) dimension array is OK )
+!
 subroutine RPN_COMM_shuf_dist(setno,  &
                               global,gni,gnj,gnk,  &
                               local,mini,maxi,minj,maxj,lnk,  &
@@ -78,7 +89,7 @@ subroutine RPN_COMM_shuf_dist(setno,  &
   contains
 !
 ! distribute one 2D array at a time
-! no column has 2 IO PES, neither has any row
+! assumption: no column has 2 IO PES, neither has any row
 !
   subroutine RPN_COMM_shuf_dist_1(setno,  &
                                   global,gni,gnj,gk,  &
@@ -99,78 +110,129 @@ subroutine RPN_COMM_shuf_dist(setno,  &
     integer, intent(IN) :: npes
     logical, intent(IN) :: periodx,periody
     integer, intent(OUT) :: status
-    logical :: on_column
-    integer :: root, ybase, kcol, ierr, halox, haloy, lni, lnj
+    logical :: on_column, error, eff_periodx
+    integer :: root, ybase, kcol, ierr, halox, haloy, lni, lnj, size2d
     integer, dimension(0:pe_nx-1) :: listofk
-    integer :: i, j
+    integer :: i, j, k, i0, in, ioff, lni0
     integer, dimension(:,:), pointer :: fullrow
     integer, dimension(:,:,:), pointer :: local_1
-    integer, dimension(0:pe_nx-1) :: cx, dx
+    integer, dimension(0:pe_nx-1) :: cxs, dxs, cxr, dxr
     integer, dimension(0:pe_ny-1) :: cy, dy
 
-    liste_o(gk) = .false.
-    on_column = .false.
+    on_column = .false.    ! precondition for failure in case a hasty exit is needed
+    status = RPN_COMM_ERROR
+    nullify(fullrow)
+    nullify(local_1)
+!
+    do i = 2 , npes
+      if(pe_x(i) <= pe_x(i-1) .or. pe_y(i) <= pe_y(i-1)) return  ! pe_x and pe_y must be monotonous and increasing
+    enddo
     root = -1 
     kcol = -1
-    haloy = minj + 1
-    halox = mini + 1
+    haloy = 1 - minj    ! halos are implicitly specified by lower bound of x and y dimensions
+    halox = 1 - mini
+    size2d = (maxj-minj+1) * (maxi-mini+1)
+    eff_periodx = periodx .and. (halox > 0)
     lni = count_x(pe_mex)
     lnj = count_y(pe_mey)
+    if(maxi < lni+halox .or. maxj < lnj+haloy) return  ! upper boun cannot accomodate halo
     do i = 1 , npes
       if(pe_mex == pe_x(i)) then
-        on_column = .true.
-        root = pe_y(i)
-        if(root == pe_mey) kcol = gk             ! level of 2D plane for this column
+        on_column = .true.                       ! there is an IO PE on the column
+        root = pe_y(i)                           ! y coordinate of IO PE, will be the root for scatterv
+        if(root == pe_mey) kcol = gk             ! level of 2D plane that this PE will distribute (it is the root)
         exit
       endif
     enddo
-!
-!   first and last PE along column get count + haloy, others get count + 2*haloy
-!
-    cy = count_y + haloy
-    cy(1:pe_ny-2) = cy(1:pe_ny-2) + haloy       ! count, adjusted for halo (periody assumed false)
-    cy = cy * gni                               ! and multiply by row length
-!
-!   all except first PE along column have their starting point bumped down by one haloy width
-!
-    dy = start_y - 1
-    dy(1:pe_ny-1) = dy(1:pe_ny-1) - haloy       ! displacement, adjusted for halo (periody assumed false)
-    dy = dy * gni                               ! and multiply by row length
-    ybase = minj
-    if(pe_mey == 0) ybase = 1                    ! south PE gets no south halo (periody assumed false)
-!
-    if(on_column)then  ! i am on a column where there is a member of the set
-      allocate(fullrow(gni,minj:maxj))
-      call mpi_bcast(kcol,1,MPI_INTEGER,root,pe_mycol,ierr)
-      call mpi_scatterv(global,cy,dy,MPI_INTEGER,   &
-                        fullrow(1,ybase),cy(pe_mey),MPI_INTEGER, &
-                        root,pe_mycol,ierr)
-    else
-      allocate(fullrow(1,1))
-    endif
-!
-!   fullrow now contains what will be redistributed along x, haloy is accounted for
-!   we may now process halo along x periodicity condition
-!
-    if(periodx .and. halox > 0)then
-      do j = minj , maxj
-        fullrow(mini:0,j)          = fullrow(lni-halox+1:lni,j)   ! east halo
-        fullrow(lni+1:lni+halox,j) = fullrow(1:halox,j)           ! west halo
-      enddo
-    endif
+    if(on_column) call mpi_bcast(kcol,1,MPI_INTEGER,root,pe_mycol,ierr)    ! send gk to all PEs on the column
 !
 !   get list of which PE has which gk along row
 !   if column had no IO PE, -1 gets transmitted
 !   if IO PE on column had nothing to contribute, 0 should get transmitted
 !
+    listofk = 0
     call mpi_allgather(kcol,1,MPI_INTEGER,listofk,1,MPI_INTEGER,pe_myrow,ierr)
+    if(maxval(listofk) <= 0) then   ! no contribution from any IO PE
+      status = RPN_COMM_OK
+      return
+    endif
+    do i = 0 , pe_nx-1
+       if(listofk(i) > 0) liste_o(listofk(i)) = .false.
+    enddo
 !
-!   we can now compute sizes and displacements for the final alltoallv
+!   first and last PE on column get count + haloy rows, others get count + 2*haloy rows
 !
+    cy = count_y + haloy
+    cy(1:pe_ny-2) = cy(1:pe_ny-2) + haloy       ! count, adjusted for halo (periody assumed false)
+    cy = cy * gni                               ! and multiply by row length
+!
+!   all except first PE on column have their starting point bumped down by one haloy width
+!
+    dy = start_y - 1
+    dy(1:pe_ny-1) = dy(1:pe_ny-1) - haloy       ! displacement, adjusted for halo (periody assumed false)
+    dy = dy * gni                               ! and multiply by row length
+    ybase = minj
+    if(pe_mey == 0) ybase = 1                   ! south PE gets no south halo (periody assumed false)
+!
+    if(on_column .and. kcol > 0)then    ! this PE is on a column where a member of the IO PE group has something to send
+      allocate(fullrow(gni,minj:maxj))
+      allocate(local_1(mini:maxi,minj:maxj,pe_nx))
+      call mpi_scatterv(global,cy,dy,MPI_INTEGER,   &          ! 
+                        fullrow(1,ybase),cy(pe_mey),MPI_INTEGER, &
+                        root,pe_mycol,ierr)
+!
+!     fullrow now contains what will be redistributed along x, haloy is accounted for
+!     we may now process reshaping and halo along x periodicity condition
+!
+      lni0 = count_x(0)
+      do k = 1 , pe_nx
+        i0 = mini
+        in = maxi
+        if(k == 1) i0 = 1
+        if(k == pe_nx) in = count_x(pe_nx-1)
+        ioff = start_x(k-1) - 1
+        do j = minj , maxj
+          local_1(  :  ,j,k) = 0
+          local_1(i0:in,j,k) = fullrow(i0+ioff:in+ioff,j)
+          if(eff_periodx .and. (k == 1)) &
+                 local_1(    1-halox:0    ,j,k) = fullrow(gni-halox+1:gni,j)   ! west halo from global east
+          if(eff_periodx .and. (k == pe_nx)) &
+                 local_1(lni0+1:lni0+halox,j,k) = fullrow(1:halox,j)           ! east halo from global west
+        enddo
+      enddo
+      cxs = size2d  ! send sizes and displacements for the final alltoallv
+      dxs(0) = 0
+      do i = 1 , pe_nx-1
+        dxs(i) = dxs(i-1) + cxs(i-1)
+      enddo
+    else
+      allocate(fullrow(1,1))
+      allocate(local_1(1,1,pe_nx))
+      cxs = 0                      ! nothing to send from here, size = displacement = 0
+      dxs = 0
+    endif
+!
+!   we can now compute receive sizes and displacements for the final alltoallv
+!
+    do i = 0 , pe_nx-1
+      if(listofk(i) > 0) then
+        cxr(i) = size2d
+        dxr(i) = (listofk(i)-1) * size2d
+      else
+        cxr(i) = 0
+        dxr(i) = 0
+      endif
+    enddo
 !   final alltoallv
 !
     local = 0
-    status = RPN_COMM_OK
+!
+    if(associated(fullrow)) deallocate(fullrow)
+    if(associated(local_1)) deallocate(local_1)
+    status = RPN_COMM_OK   ! success at last !
+    do i = 0 , pe_nx-1
+       if(listofk(i) > 0) liste_o(listofk(i)) = .true.
+    enddo
   end subroutine RPN_COMM_shuf_dist_1
 end subroutine RPN_COMM_shuf_dist
 !
