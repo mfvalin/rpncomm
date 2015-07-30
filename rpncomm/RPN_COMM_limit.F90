0de4a5a48d79863cf90f073c6e176b20a20933ad
Michel Valin <mfvalin@gmail.com>
2015-07-30 13:16:02 -0400

    slight refactoring + more comments

--- rpncomm/RPN_COMM_limit.F90
+++ rpncomm/RPN_COMM_limit.F90
@@ -5,7 +5,7 @@ stop
 end
 #endif
 !/! RMNLIB - Library of useful routines for C and FORTRAN programming
-! ! Copyright (C) 1975-2012  Division de Recherche en Prevision Numerique
+! ! Copyright (C) 1975-2015  Recherche en Prevision Numerique
 ! !                          Environnement Canada
 ! !
 ! ! This library is free software; you can redistribute it and/or
@@ -34,46 +34,79 @@ end
 
       gmin = 1
       gmax = 13
-      print 101,'NPE  =',NPE
-      print 101,'relax =',3
-      do my_id=0,NPE-1
-      status = RPN_COMM_limit_2(my_id, npe, gmin, gmax,lmini,lmaxi,count, offset,3)
+      print 101,'NPE, gmin, gmax :',NPE,gmin,gmax
+      print 101,'relax =',0
+      status = RPN_COMM_limit_2(0, npe, gmin, gmax,lmini,lmaxi,count, offset,0)
       print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax',' stat'
-      print 101, 'pe_me=',my_id, lmini,lmaxi, gmin, gmax, status
+      print 101, 'pe_me=',0, lmini,lmaxi, gmin, gmax, status
       print 101, 'count=',count
       print 101, 'offst=',offset
-      print *,''
+      print *, '---------------------------'
+      print 101,'NPE, gmin, gmax :',NPE,gmin,gmax
+      print 101,'relax =',1
+      status = RPN_COMM_limit_2(0, npe, gmin, gmax,lmini,lmaxi,count, offset,1)
+      print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax',' stat'
+      print 101, 'pe_me=',0, lmini,lmaxi, gmin, gmax, status
+      print 101, 'count=',count
+      print 101, 'offst=',offset
+      print *, '---------------------------'
+      print 101,'NPE, gmin, gmax :',NPE,gmin,gmax
+      print 101,'relax =',2
+      status = RPN_COMM_limit_2(0, npe, gmin, gmax,lmini,lmaxi,count, offset,2)
+      print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax',' stat'
+      print 101, 'pe_me=',0, lmini,lmaxi, gmin, gmax, status
+      print 101, 'count=',count
+      print 101, 'offst=',offset
+      print *, '---------------------------'
+      gmin = 1
+      gmax = 13
+      print 101,'NPE, gmin, gmax :',NPE,gmin,gmax
+      print 101,'relax =',3
+      do my_id=0,NPE-1
+        status = RPN_COMM_limit_2(my_id, npe, gmin, gmax,lmini,lmaxi,count, offset,3)
+        print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax',' stat'
+        print 101, 'pe_me=',my_id, lmini,lmaxi, gmin, gmax, status
+        print 101, 'count=',count
+        print 101, 'offst=',offset
+        print *,''
       enddo
       print *, '---------------------------'
-101   format(A7,10I6)
-102   format(A7,10A6)
+101   format(A,10I6)
+102   format(A,10A6)
       my_id=NPE-1
       status = RPN_COMM_limit_2(my_id, npe, gmin, gmax,lmini,lmaxi,count, offset,1)
       print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax','relax',' stat'
       print 101, 'pe_me=',my_id, lmini,lmaxi, gmin, gmax, 1, status
       print 101, 'count=',count
       print 101, 'offst=',offset
-      print *,''
+      print *, '---------------------------'
       gmax = 5
       status = RPN_COMM_limit_2(my_id, npe, gmin, gmax,lmini,lmaxi,count, offset,1)
       print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax','relax',' stat'
       print 101, 'pe_me=',my_id, lmini,lmaxi, gmin, gmax, 1, status
       print 101, 'count=',count
       print 101, 'offst=',offset
-      print *,''
+      print *, '---------------------------'
+      gmax = 5
+      status = RPN_COMM_limit_2(my_id, npe, gmin, gmax,lmini,lmaxi,count, offset,2)
+      print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax','relax',' stat'
+      print 101, 'pe_me=',my_id, lmini,lmaxi, gmin, gmax, 2, status
+      print 101, 'count=',count
+      print 101, 'offst=',offset
+      print *, '---------------------------'
       status = RPN_COMM_limit_2(my_id, npe, gmin, gmax,lmini,lmaxi,count, offset,3)
       print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax','relax',' stat'
       print 101, 'pe_me=',my_id, lmini,lmaxi, gmin, gmax, 3, status
       print 101, 'count=',count
       print 101, 'offst=',offset
-      print *,''
+      print *, '---------------------------'
       gmax = 8
       status = RPN_COMM_limit_2(my_id, npe, gmin, gmax,lmini,lmaxi,count, offset,3)
       print 102, '      ','   ID',' mini',' maxi',' gmin',' gmax','relax',' stat'
       print 101, 'pe_me=',my_id, lmini,lmaxi, gmin, gmax, 3, status
       print 101, 'count=',count
       print 101, 'offst=',offset
-      print *,''
+      print *, '---------------------------'
       end subroutine RPN_COMM_test_limit
 !**function  RPN_COMM_limit_2 global domain decomposition function (along one dimension)
 !InTf!
@@ -96,12 +129,13 @@ end
 !  I    relax  decomposition mode
 !          0 : strict mode, all tiles but last one must have same dimension, 
 !              last tile may be shorter but may not have zero dimension
-!          1 : some tiles at end 1 shorter than tiles at beginning, zero dimension not allowed
-!          2 : same as relax=1 but zero dimension tiles at end are allowed
+!          1 : some tiles at end 1 shorter than tiles at beginning, zero size not allowed for these tiles
+!          2 : same as relax=1 but zero dimension tiles at end are allowed (usefule only if more PEs than points)
 !          3 : tiles with same length followed by a shorter tile followed by zero size tiles
 !
 !notes
 !     this function is totally stand alone and could eventually be moved into the rmnlib library
+!     mode 2 only makes sense when more PEs than points
 !*
       integer, intent(IN) ::  my_id, npe, gmin, gmax, relax         !InTf!
       integer, intent(OUT) :: lmini,lmaxi                           !InTf!
@@ -123,30 +157,37 @@ end
         offset(i) = offset(i-1) + count(i-1)
         val2 = val2 + count(i)
       enddo
-      if(gtot < npe .and. relax < 2) then   ! there would be zero sized tiles and relax is not 2 or 3
-          goto 777
+!
+      if(gtot < npe) then                   ! less points than PEs, there will be zero sized tiles at end and relax is 0 or 1
+          if( relax < 2) goto 777           ! ERROR
+          goto 666                          ! count is OK, go compute offset if relax >= 2
       end if
-      if(relax == 3) goto 666               ! 
+!
+      if(relax == 3) goto 666               ! count is OK, go compute offset
+!
       val2 = gtot - (npe-1)*val1            ! potential size of last "tile" in strict mode
-      if (val2 <= 0  .and. relax == 0)  then ! in STRICT mode last "tile" would have a negative size
-          goto 777
-      end if
-      if(val2 <= 0 ) val1 = gtot/npe        ! relax distribution rule, try mode 1
-      count = val1
+      if (val2 <= 0) then                   ! in STRICT mode last "tile" would have a negative size
+        if(relax == 0) goto 777             ! STRICT mode, and last "tile" would have a negative size, ERROR
+        val1 = gtot/npe                     ! relaxed distribution rule, try mode 1 with val1 = floor(gtot/npe)
+      endif
+!
+      count = val1                          ! [ceiling or floor](gtot/npe)
       if(val2 > 0) then                     ! strict mode will work, use it
          count(npe) = val2
-      else                                  ! relaxed mode (1 or 2)
-        do i=1,mod(gtot,npe)                ! add 1 to the size of some tiles at beginning
+      else                                  ! relaxed mode (1 or 2) (val2 <= 0 in mode 0 case already taken care of)
+        do i=1,mod(gtot,npe)                ! add 1 to the size of some tiles at beginning to distribute leftovers
           count(i)=count(i)+1
         end do
       end if
-666   do i= 2, npe   ! use count table to compute offsets from beginning
+!                                             we are now ready to compute offset using values in count
+666   do i= 2, npe                          ! use count table to compute offsets from beginning (ORIGIN 0)
          offset(i) = offset(i-1) + count(i-1)
-      end do 
+      enddo
+!
       lmini = gmin + offset(my_id+1)             ! start of "tile" in global space
       lmaxi = min(gmax,lmini+count(my_id+1)-1)   ! end of "tile" in global space
 
-      RPN_COMM_limit_2 = 0  ! SUCCESS
+      RPN_COMM_limit_2 = 0                  ! SUCCESS
       return
 
 777   continue      ! something bad happened, print error message and return error status
