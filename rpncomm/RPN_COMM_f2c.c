f39bf6494fd3941a23d1f6ef4fee690f945df5a4
Michel Valin <mfvalin@gmail.com>
2015-08-09 16:28:37 -0400

    adjusted C interface and test to get better interface from fortran

--- rpncomm/RPN_COMM_f2c.c
+++ rpncomm/RPN_COMM_f2c.c
@@ -21,9 +21,9 @@ MPI_Fint RPN_COMM_comm_c2f( F_C_univ *in) {
   return MPI_Comm_c2f(in->all.comm);
 }
 
-F_C_union RPN_COMM_comm_f2c( F_C_univ *in) {
+F_C_union RPN_COMM_comm_f2c( F_C_univ *in, MPI_Fint comm) {
   F_C_univ temp;
-  MPI_Fint comm = in->t2;
+  in->t2 = comm;
   temp.all.comm = MPI_Comm_f2c(comm);
   return temp.all;
 }
@@ -32,9 +32,9 @@ MPI_Fint RPN_COMM_type_c2f( F_C_univ *in) {
   return MPI_Type_c2f(in->all.type);
 }
 
-F_C_union RPN_COMM_type_f2c( F_C_univ *in) {
+F_C_union RPN_COMM_type_f2c( F_C_univ *in, MPI_Fint type) {
   F_C_univ temp;
-  MPI_Fint type = in->t2;
+  in->t2 = type;
   temp.all.type = MPI_Type_f2c(type);
   return temp.all;
 }
@@ -43,9 +43,9 @@ MPI_Fint RPN_COMM_group_c2f( F_C_univ *in) {
   return MPI_Group_c2f(in->all.group);
 }
 
-F_C_union RPN_COMM_group_f2c( F_C_univ *in) {
+F_C_union RPN_COMM_group_f2c( F_C_univ *in, MPI_Fint group) {
   F_C_univ temp;
-  MPI_Fint group = in->t2;
+  in->t2 = group;
   temp.all.group = MPI_Group_f2c(group);
   return temp.all;
 }
@@ -54,9 +54,9 @@ MPI_Fint RPN_COMM_file_c2f( F_C_univ *in) {
   return MPI_File_c2f(in->all.file);
 }
 
-F_C_union RPN_COMM_file_f2c( F_C_univ *in) {
+F_C_union RPN_COMM_file_f2c( F_C_univ *in, MPI_Fint file) {
   F_C_univ temp;
-  MPI_Fint file = in->t2;
+  in->t2 = file;
   temp.all.file = MPI_File_f2c(file);
   return temp.all;
 }
@@ -65,9 +65,9 @@ MPI_Fint RPN_COMM_info_c2f( F_C_univ *in) {
   return MPI_Info_c2f(in->all.info);
 }
 
-F_C_union RPN_COMM_info_f2c( F_C_univ *in) {
+F_C_union RPN_COMM_info_f2c( F_C_univ *in, MPI_Fint info) {
   F_C_univ temp;
-  MPI_Fint info = in->t2;
+  in->t2 = info;
   temp.all.info = MPI_Info_f2c(info);
   return temp.all;
 }
@@ -76,9 +76,9 @@ MPI_Fint RPN_COMM_op_c2f( F_C_univ *in) {
   return MPI_Op_c2f(in->all.op);
 }
 
-F_C_union RPN_COMM_op_f2c( F_C_univ *in) {
+F_C_union RPN_COMM_op_f2c( F_C_univ *in, MPI_Fint op) {
   F_C_univ temp;
-  MPI_Fint op = in->t2;
+  in->t2 = op;
   temp.all.op = MPI_Op_f2c(op);
   return temp.all;
 }
@@ -87,9 +87,9 @@ MPI_Fint RPN_COMM_request_c2f( F_C_univ *in) {
   return MPI_Request_c2f(in->all.request);
 }
 
-F_C_union RPN_COMM_request_f2c( F_C_univ *in) {
+F_C_union RPN_COMM_request_f2c( F_C_univ *in, MPI_Fint request) {
   F_C_univ temp;
-  MPI_Fint request = in->t2;
+  in->t2 = request;
   temp.all.request = MPI_Request_f2c(request);
   return temp.all;
 }
@@ -98,9 +98,9 @@ MPI_Fint RPN_COMM_window_c2f( F_C_univ *in) {
   return MPI_Win_c2f(in->all.win);
 }
 
-F_C_union RPN_COMM_window_f2c( F_C_univ *in) {
+F_C_union RPN_COMM_window_f2c( F_C_univ *in, MPI_Fint win) {
   F_C_univ temp;
-  MPI_Fint win = in->t2;
+  in->t2 = win;
   temp.all.win = MPI_Win_f2c(win);
   return temp.all;
 }
