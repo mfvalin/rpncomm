6c76c9c388459d77634c0d53d5e4d0a07ecb7f60
Michel Valin <mfvalin@gmail.com>
2015-08-24 11:27:40 -0400

    added -h/--help and comments

--- tools/mk.interfaces.pl
+++ tools/mk.interfaces.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/env perl
 
-# Si pas d'arguments, on prend STDIN
-if ( $#ARGV > -1 ){
+if ( $#ARGV > -1 ){   # collect arguments (nems of files to process
  @listfile = (@ARGV) ;
  $listfile[$#listfile] =~ s/\n/ /;
 }
 
 foreach $file (@listfile) {
-
+  if($file =~ /^-h|--help/) {
+    print STDERR "usage : mk.interfaces.pl [-h|--help] list of file names\n";
+    exit;
+  }
   if (! open(INPUT,"<$file") ) {
       print STDERR "Can't open input file $file.\n";
       next;}
@@ -21,7 +23,7 @@ foreach $file (@listfile) {
       $line = $1 ;
       if($line  =~ /!!(.*)/) { 
         $line = $1;
-      }
+      }   # lines starting with !!
       if($line  =~ /^!VOID[\$].*[ ]([a-zA-Z][a-zA-Z_0-9]*)[ ]*$/) {   # special IGNORE TKR
         print "# if __GNUC__  > 3 && __GNUC_MINOR__ > 7\n";           # gfortran 4.8 and up
         print" type(*), dimension(..) :: $1\n";                       # deferred type and rank
@@ -40,7 +42,7 @@ foreach $file (@listfile) {
         print "# endif\n";
         print"#endif\n";
         next;
-      }
+      }   # !VOID$
       $line =~ s/^\s+// ; $line =~ s/\s+$// ;   # trim space at both ends
       $tail = '&' ; $head = "      " ;
 
@@ -53,8 +55,8 @@ foreach $file (@listfile) {
         $l = length "$line";
       }
       print "$head$line\n";
-    }
-  }
+    }   # !InTf!
+  }   # <INPUT>
 
   print "#endif\n"
 }
