972524181f7df934048b3e95778b83550dda4ab7
Michel Valin <mfvalin@gmail.com>
2015-11-27 17:51:57 -0500

    added generic interface RPN_COMM_is_null to test for uninitialized types

--- rpncomm/TEST_000.F90
+++ rpncomm/TEST_000.F90
@@ -6,6 +6,39 @@
 	call mpi_comm_size(MPI_COMM_WORLD,totpes,ierr)
         call mpi_comm_rank(MPI_COMM_WORLD,myrank,ierr)
 	print *,'I am PE ',myrank+1,' of ',totpes
+	call test_null
 	call mpi_finalize(ierr)
 	stop
 	end
+subroutine test_null
+  use ISO_C_BINDING
+  implicit none
+  include 'RPN_COMM_types.inc'
+  include 'RPN_COMM_is_null.inc'
+  type(rpncomm_context) :: a
+  type(rpncomm_ptr) :: b
+  type(rpncomm_window) :: c
+  type(rpncomm_file) :: d
+  type(rpncomm_request) :: e
+  type(rpncomm_datatype) :: f
+  type(rpncomm_communicator) :: g
+  type(rpncomm_group) :: h
+  a%p = C_NULL_PTR
+  b%p = C_NULL_PTR
+  c%p = C_NULL_PTR
+  d%p = C_NULL_PTR
+  e%p = C_NULL_PTR
+  f%p = C_NULL_PTR
+  g%p = C_NULL_PTR
+  h%p = C_NULL_PTR
+  print *,RPN_COMM_is_null(a)
+  print *,RPN_COMM_is_null(b)
+  print *,RPN_COMM_is_null(c)
+  print *,RPN_COMM_is_null(d)
+  print *,RPN_COMM_is_null(e)
+  print *,RPN_COMM_is_null(f)
+  print *,RPN_COMM_is_null(g)
+  print *,RPN_COMM_is_null(h)
+return
+end
+	
