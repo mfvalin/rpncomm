8017e6d1cd6dd06988a00ca67c4b2dbea83ed121
Michel Valin <mfvalin@gmail.com>
2016-01-11 14:24:06 -0500

    introduit verificateur de dependances de Stephane

--- rpncomm/rpn_comm.mk
+++ rpncomm/rpn_comm.mk
@@ -64,13 +64,16 @@ $(VPATH)/RPN_COMM_interfaces_int.inc: $(wildcard $(VPATH)/RPN_COMM*.?90) $(wildc
 #                          $(VPATH)/RPN_COMM_interfaces_int.inc $(VPATH)/RPN_COMM_interfaces.inc
 $(VPATH)/dependencies.mk:
 	rm -f $(VPATH)/dependencies.mk $(TMPDIR)/dependencies+.mk
-	-which gnu_find 2>/dev/null 1>/dev/null || (cd $(VPATH) ; find . -maxdepth 1 -type f | grep RPN_COMM | grep -v TEST_0 | ../tools/mk.dependencies.pl >$(TMPDIR)/dependencies+.mk ) || true
-	-(which gnu_find 2>/dev/null 1>/dev/null && (cd $(VPATH) ; gnu_find . -maxdepth 1 -type f | grep RPN_COMM | grep -v TEST_0 ../tools/mk.dependencies.pl >$(TMPDIR)/dependencies+.mk )) || true
+#	-which gnu_find 2>/dev/null 1>/dev/null || (cd $(VPATH) ; find . -maxdepth 1 -type f | grep RPN_COMM | grep -v TEST_0 | ../tools/mk.dependencies.pl >$(TMPDIR)/dependencies+.mk ) || true
+#	-(which gnu_find 2>/dev/null 1>/dev/null && (cd $(VPATH) ; gnu_find . -maxdepth 1 -type f | grep RPN_COMM | grep -v TEST_0 ../tools/mk.dependencies.pl >$(TMPDIR)/dependencies+.mk )) || true
+	(cd $(VPATH) ; ../tools/rdedep.pl --flat_layout --out=$(TMPDIR)/dependencies+.mk $$(ls -1 RPN_COMM* | grep -v TEST_0)) || true
 	mv $(TMPDIR)/dependencies+.mk $(VPATH)/dependencies.mk
 	echo === touch  $(VPATH)/dependencies.mk ===
 	touch  $(VPATH)/dependencies.mk
 #
-dep: $(VPATH)/dependencies.mk 
+dep_rm:
+	rm -f $(VPATH)/dependencies.mk
+dep: dep_rm $(VPATH)/dependencies.mk 
 #	rm -f $(VPATH)/dependencies.mk $(TMPDIR)/dependencies+.mk
 #	-which gnu_find 2>/dev/null 1>/dev/null || (cd $(VPATH) ; find . -maxdepth 1 -type f | grep RPN_COMM | grep -v TEST_0 | ../tools/mk.dependencies.pl >$(TMPDIR)/dependencies+.mk ) || true
 #	-(which gnu_find 2>/dev/null 1>/dev/null && (cd $(VPATH) ; gnu_find . -maxdepth 1 -type f | grep RPN_COMM | grep -v TEST_0 ../tools/mk.dependencies.pl >$(TMPDIR)/dependencies+.mk )) || true
