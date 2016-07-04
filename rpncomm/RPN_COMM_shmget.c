44f434b625152abf0fdc8038480d78840ee1247a
Michel Valin <mfvalin@gmail.com>
2016-07-04 16:09:59 -0400

    new rebind code + extended shared memory get

--- rpncomm/RPN_COMM_shmget.c
+++ rpncomm/RPN_COMM_shmget.c
@@ -15,33 +15,39 @@
         function rpn_comm_shmget(comm,size) result(where) bind(C,name='F_rpn_comm_shmget')    !InTf!
         import C_INT, C_PTR                                   !InTf!
         implicit none                                         !InTf!
-          integer(C_INT), intent(IN), value :: comm           !InTf!   ! RPN_COMM communicator
-          integer(C_INT), intent(IN), value :: size           !InTf!   ! size in bytes of shared memory area
+          integer(C_INT), intent(IN), value :: comm           !InTf!   ! Fortran communicator (all members MUST be on same SMP node if size > 0)
+          integer(C_INT), intent(IN), value :: size           !InTf!   ! size in bytes of shared memory area (if <0, split communicator to SMP node)
           type(C_PTR) :: where                                !InTf!   ! pointer to shared memory area
         end function rpn_comm_shmget                          !InTf!
 #endif
 
-void *F_rpn_comm_shmget(int comm, unsigned int shm_size)  /* allocate a shared memory segment */
+
+void *C_rpn_comm_shmget(MPI_Comm c_comm_in, unsigned int shm_size)  /* allocate a shared memory segment */
 {
-  size_t size=shm_size;                                   /* size of shared memory segment in bytes */
+ MPI_Comm c_comm;
+  size_t size;                                   /* size of shared memory segment in bytes */
   int id;
   struct shmid_ds shm_buf;
   void *ptr;
   int ierr, myrank, myhost, myhost2;
-  MPI_Fint f_comm=comm;                                   /* all members of this communicator MUST be on same node */
-  MPI_Comm c_comm;
   
-
-  c_comm = MPI_Comm_f2c(f_comm);  /* translate Fortran communicator into C communicator */
-
   myhost=gethostid();
-  ierr=MPI_Allreduce(&myhost,&myhost2,1,MPI_INTEGER,MPI_BOR,c_comm); /* boolean OR from all members of this comunicator */
-  myhost ^= myhost2;                                                 /* should be zero everywhere */
-  ierr=MPI_Allreduce(&myhost,&myhost2,1,MPI_INTEGER,MPI_BOR,c_comm); /* boolean OR from all members of this comunicator */
-  if(0 != myhost2){                                                  /* not zero : ERROR */
-    if(myrank == 0) fprintf(stderr,"ERROR: (rpn_comm_shmget) processes are not all on same node \n");
-    return NULL;    /* error */
-    }
+  if(shm_size > 0){
+    size = shm_size;
+    c_comm = c_comm_in;                   /* all members of this communicator MUST be on same SMP node */
+    ierr=MPI_Allreduce(&myhost,&myhost2,1,MPI_INTEGER,MPI_BOR,c_comm); /* boolean OR from all members of this comunicator */
+    myhost ^= myhost2;                                                 /* should be zero everywhere */
+    ierr=MPI_Allreduce(&myhost,&myhost2,1,MPI_INTEGER,MPI_BOR,c_comm); /* boolean OR from all members of this comunicator */
+    if(0 != myhost2){                                                  /* not zero : ERROR */
+      if(myrank == 0) fprintf(stderr,"ERROR: (rpn_comm_shmget) processes are not all on same node \n");
+      return NULL;    /* error */
+      }
+  }else{    // make c_comm from c_comm_in
+    size = -shm_size;
+    ierr = MPI_Comm_rank(c_comm_in,&myrank);
+    if(myhost < 0) myhost = -myhost;                          /* color must be positive for MPI_Comm_split */
+    ierr = MPI_Comm_split(c_comm_in,myhost,myrank,&c_comm);   /* split input communicator so that all members are on same SMP node*/
+  }
 
   ierr=MPI_Comm_rank(c_comm,&myrank);
   if(myrank == 0) id=shmget(IPC_PRIVATE,size,IPC_CREAT|S_IRUSR|S_IWUSR);  /* rank 0 allocates shared memory segment */
@@ -53,14 +59,13 @@ void *F_rpn_comm_shmget(int comm, unsigned int shm_size)  /* allocate a shared m
     if(myrank == 0) printf("INFO: (rpn_comm_shmget) created shared memory segment of size %d\n",shm_size);
   }
 
-//  printf("id = %d %x \n",id,id);
   ptr=shmat(id,NULL,0);                                                   /* all processes attach memory segment */
   if(ptr == NULL) printf("ERROR: (rpn_comm_shmget) got a null pointer from shmat, process = %d\n",myrank);
   myhost = (ptr == NULL);   /* this better be zero */
-//  if(ptr != NULL) printf("INFO: (rpn_comm_shmget) process %d attached to segment\n",myrank);
+//  if(ptr != NULL) printf("DEBUG: (rpn_comm_shmget) process %d attached to segment\n",myrank);
+//  printf("DEBUG: addr = %16p\n",ptr);
   ierr=MPI_Allreduce(&myhost,&myhost2,1,MPI_INTEGER,MPI_BOR,c_comm); /* boolean OR from all members of this comunicator */
 //  ierr=MPI_Barrier(c_comm);                                         /* all processes have attached the segment */
-//  printf("addr = %16p\n",ptr);
 
   if(myrank == 0) shmctl(id,IPC_RMID,&shm_buf);      /* mark segment for deletion to make sure it is released when all processes terminate */
   if(0 != myhost2){                                                  /* not zero : OUCH */
@@ -68,5 +73,11 @@ void *F_rpn_comm_shmget(int comm, unsigned int shm_size)  /* allocate a shared m
     return NULL;    /* error */
     }
   return ptr;                                        /* return pointer tio shared memory area */
+}
+
+void *F_rpn_comm_shmget(MPI_Fint f_comm, unsigned int shm_size)  /* allocate a shared memory segment */
+{
+//  MPI_Fint f_comm=comm;                                   /* all members of this communicator MUST be on same SMP node */
+  return(C_rpn_comm_shmget(MPI_Comm_f2c(f_comm), shm_size));  /* translate Fortran communicator into C communicator before call */
 }
 
--- rpncomm/RPN_COMM_unbind_process.c
+++ rpncomm/RPN_COMM_unbind_process.c
@@ -19,11 +19,13 @@
  */
 #if defined(linux)
 #define _GNU_SOURCE
+#include <stdint.h>
 #endif
 #include <sched.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <mpi.h>
 
 /*
    unset any processor affinity for the current process
@@ -41,32 +43,116 @@
 */
 
 /* use pragma weak to create alternate FORTRAN entry points */
-#pragma weak rpn_comm_unbind_process_=rpn_comm_unbind_process
-#pragma weak rpn_comm_unbind_process__=rpn_comm_unbind_process
-
-
 /* WARNING: some old versions of gcc do not generate the weak entry points correctly */
 
+#if defined(linux)
+static cpu_set_t set;
+static int lo_core=-1;
+static int hi_core=-1;
+#endif
+
+#if defined(linux)
+static void X86_cpuid(uint32_t eax, uint32_t ecx, uint32_t* regs)  /* interface to x86 cpuid instruction */
+{
+#if defined(__x86_64__) || defined( __i386__ )
+    uint32_t ebx, edx;
+# if defined( __i386__ ) && defined ( __PIC__ )
+     /* PIC under 32-bit EBX must not be clobbered */
+    __asm__ ( "movl %%ebx, %%edi \n\t cpuid \n\t xchgl %%ebx, %%edi" : "=D" (ebx),
+# else
+   ebx = 0;
+    __asm__ ( "cpuid" : "+b" (ebx),
+# endif
+              "+a" (eax), "+c" (ecx), "=d" (edx) );
+    regs[0] = eax; regs[1] = ebx; regs[2] = ecx; regs[3] = edx;
+#else
+    regs[0] = 0; regs[1] = 0; regs[2] = 0; regs[3] = 0;
+#endif
+}     
+#endif
+
+#pragma weak rpn_comm_rebind_process__=rpn_comm_rebind_process
+#pragma weak rpn_comm_rebind_process_=rpn_comm_rebind_process
+void rpn_comm_rebind_process__(void);
+void rpn_comm_rebind_process_(void);
+void rpn_comm_rebind_process(void)  /* rebind thread(s) of MPI rank */
+{
+#if defined(linux)
+  char *omp;
+  int nthreads;
+  int ncores, numa, size, rank, i, color, threadpercore, cpucores;
+  MPI_Comm comm;
+  int t[512];
+  uint32_t regs[4];
+
+  if(lo_core == -1) {   /* first call must be by thread 0 in single thread mode */
+    nthreads = 1;
+    omp = getenv("OMP_NUM_THREADS");
+    if(omp != NULL) nthreads=atoi(omp);
+
+    X86_cpuid( 0x0B, 0, regs );
+    threadpercore = regs[1] & 0xFFFF;        /* virtual threads per core */
+    if(threadpercore < 1) threadpercore = 1;
+    X86_cpuid( 0x0B, 1, regs );
+    cpucores = regs[1] & 0xFFFF;
+    cpucores /= threadpercore;               /* "real" cores */
+    if(cpucores < 1) cpucores = 1;
+    ncores=sysconf(_SC_NPROCESSORS_CONF);    /* number of cores in node */
+    ncores /= threadpercore;                 /* hyperthread factor */
+    numa = cpucores;                         /* number of cores per socket */
+
+    color=gethostid();                       /* host id */
+    color = (color > 0) ? color : (-color);  /* make color positive */
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_split(MPI_COMM_WORLD,color,rank,&comm);
+    MPI_Comm_size(comm, &size);              /* size ranks on SMP node */
+    MPI_Comm_rank(comm, &rank);              /* my rank on SMP node */
+
+    t[0] = 0;
+    for(i=1;i<=size;i++) {
+      t[i] = t[i-1] + t[i];
+      if(t[i] < numa && t[i] + t[i+1] > numa) t[i]=numa;   /* do no straddle sockets */
+      if(t[i] > ncores) t[i] = ncores;                     /* min(t[i],ncores) */
+    }
+    CPU_ZERO(&set);
+    for(i=t[rank] ; i < t[rank+1] ; i++) { CPU_SET(i,&set) ;}
+    lo_core = t[rank];
+    hi_core = t[rank+1]-1;
+  }
+  sched_setaffinity(0,sizeof(set),&set);   /* set thread affinity range */
+  
+#endif
+return;
+}
+
+#pragma weak rpn_comm_unbind_process__=rpn_comm_unbind_process
+#pragma weak rpn_comm_unbind_process_=rpn_comm_unbind_process
 void rpn_comm_unbind_process__(void);
 void rpn_comm_unbind_process_(void);
-void rpn_comm_unbind_process(void)
+void rpn_comm_unbind_process(void)    /* unbind all processes/threads */
 {
 #if defined(linux)
 
 cpu_set_t set;
 int i;
 int will_print=1;
-char *ompi=getenv("OMPI_COMM_WORLD_RANK");  /* openmpi */
+char *ompi;
 int ncores=sysconf(_SC_NPROCESSORS_CONF);
 int nthreads=1;
 int nbound=0;
 char *omp=getenv("OMP_NUM_THREADS");
+int rank;
 
 if(omp != NULL) nthreads=atoi(omp);  /* OMP_NUM_THREADS value */
 
-if(ompi == NULL) ompi=getenv("PMI_RANK");   /* mpich family */
+//                 ompi = getenv("OMPI_COMM_WORLD_RANK");  /* openmpi */
+// if(ompi == NULL) ompi = getenv("PMI_RANK");              /* mpich family */
+// if(ompi == NULL) ompi = getenv("ALPS_APP_PE");           /* ALPS/mpich family */
+// 
+// if(ompi != NULL) if(0 != atoi(ompi)) will_print=0;  /* not MPI process 0, no message */
 
-if(ompi != NULL) if(0 != atoi(ompi)) will_print=0;  /* not MPI process 0, no message */
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);   /* get MPI rank */
+will_print = (rank == 0);
 
 CPU_ZERO(&set);
 sched_getaffinity(0,sizeof(set),&set);
@@ -76,13 +162,13 @@ while(--i >=0) { if (CPU_ISSET(i,&set)){ nbound++; } }  /* how many cores are we
 if(getenv("FULL_UNBIND") != NULL) nbound = 0;  /* FULL_UNBIND variable defined, unbind no matter what */
   
 if(nthreads > nbound) {  /* need more threads than cores we can run on , unbind everything */
-  if(will_print) printf("FULL unbinding will be done, cores=%d, threads needed=%d, usable cores=%d\n",ncores,nthreads,nbound);
+  if(will_print) printf("INFO: full unbinding will be done, cores=%d, threads needed=%d, usable cores=%d\n",ncores,nthreads,nbound);
   CPU_ZERO(&set);
   i=ncores;
   while(--i >=0) { CPU_SET(i,&set);}
   sched_setaffinity(0,sizeof(set),&set);  /* set affinity to all cores */
 }else{
-  if(will_print) printf("NO unbinding will be done\n");  /* enough resources available and no forced unbind */
+  if(will_print) printf("INFO: no unbinding will be done\n");  /* enough resources available and no forced unbind */
 }
 
 #endif
