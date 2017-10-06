d53961a9091788ab8bbe0fea5240f27d4b86a084
Michel Valin <mfvalin@gmail.com>
2017-10-05 20:12:24 -0400

    added license

--- rpncomm/RPN_COMM_progatate_boundary.F90
+++ rpncomm/RPN_COMM_progatate_boundary.F90
@@ -1,3 +1,22 @@
+!/! RPN_COMM - Library of useful routines for C and FORTRAN programming
+! ! Copyright (C) 1975-2017  Division de Recherche en Prevision Numerique
+! !                          Environnement Canada
+! !
+! ! This library is free software; you can redistribute it and/or
+! ! modify it under the terms of the GNU Lesser General Public
+! ! License as published by the Free Software Foundation,
+! ! version 2.1 of the License.
+! !
+! ! This library is distributed in the hope that it will be useful,
+! ! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! ! Lesser General Public License for more details.
+! !
+! ! You should have received a copy of the GNU Lesser General Public
+! ! License along with this library; if not, write to the
+! ! Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+! ! Boston, MA 02111-1307, USA.
+! !/
 subroutine RPN_COMM_progatate_boundary(f,minx,maxx,miny,maxy,lni,lnj,nk,hx,hy)
   use rpn_comm
   implicit none
