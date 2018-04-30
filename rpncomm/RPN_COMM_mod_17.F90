b5f8c0128900e2844ebd9e6c5ea0bcd7877f943e
M. Valin <mfvalin@gmail.com>
2018-04-30 10:44:48 -0400

    temporarily keep module rpn_comm same as 4.5.16

---
+++ rpncomm/RPN_COMM_mod_17.F90
@@ -0,0 +1,24 @@
+!/* RPN_COMM - Library of useful routines for C and FORTRAN programming
+! * Copyright (C) 1975-2012  Division de Recherche en Prevision Numerique
+! *                          Environnement Canada
+! *
+! * This library is free software; you can redistribute it and/or
+! * modify it under the terms of the GNU Lesser General Public
+! * License as published by the Free Software Foundation,
+! * version 2.1 of the License.
+! *
+! * This library is distributed in the hope that it will be useful,
+! * but WITHOUT ANY WARRANTY; without even the implied warranty of
+! * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! * Lesser General Public License for more details.
+! *
+! * You should have received a copy of the GNU Lesser General Public
+! * License along with this library; if not, write to the
+! * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+! * Boston, MA 02111-1307, USA.
+! */
+module rpn_comm_17
+  use iso_c_binding
+  implicit none
+  integer :: pe_grid_host0, pe_me_grid_host0        ! PEs with same rank in pe_grid_host (same grid, same node, same rank)
+end module rpn_comm_17
