3e28b930dbd38a8682c14aa7a31feb6313649b04
Michel Valin <mfvalin@gmail.com>
2018-05-17 16:15:06 -0400

    better messages

--- rpncomm/RPN_COMM_io_server.F90
+++ rpncomm/RPN_COMM_io_server.F90
@@ -1,5 +1,5 @@
       ! pmem is array pointing to shared memory segment
-      ! 4            : number of values in array
+      ! 104          : number of values in array + version_number * 100
       ! pe_server_id : server kind
       ! pe_grid      : communicator for servers
       ! pe_me_grid   : server ordinal in pe_grid
@@ -9,10 +9,14 @@
         implicit none
         character(len=1), intent(INOUT), dimension(*) :: pmem
         integer, intent(IN), dimension(*) :: parms
-        print *,'entering IO server',parms(4)
-        if(parms(1) .ne. 4) then
-          print *,'ERROR: wrong version of rpn_comm_init calling this test IO server'
+        integer :: pe_me, ierr
+        if(parms(1) .ne. 1004) then
+          print *,'ERROR: version mismatch between rpn_comm_init and the IO server'
+          print *,'       ( expected',1004,' got',parms(1),' )'
           return
         endif
+        call MPI_comm_rank(parms(3),pe_me,ierr)
+        print 100,'entering IO server type ',parms(2),', global server rank= ',parms(4)
+100     format(A,I2,A,I3)
         return
       end subroutine RPN_COMM_io_server
