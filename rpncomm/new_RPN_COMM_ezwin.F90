cfc3102c5ff6baf99405fefdec2a80a833a8b257
Michel Valin <mfvalin@gmail.com>
2019-01-17 18:11:56 -0500

    fixed mistyped code, added tests

--- rpncomm/new_RPN_COMM_ezwin.F90
+++ rpncomm/new_RPN_COMM_ezwin.F90
@@ -24,14 +24,15 @@ module rpn_comm_ezwin_mod
     status = .false.
   end function is_invalid
 end module
-subroutine RPN_COMM_ezwin_create(sz,comm,mywin,ierr)
+subroutine RPN_COMM_ezwin_create(sz,comm,mywin,ierr)       !InTf!
   use rpn_comm_ezwin_mod
   implicit none
   include 'mpif.h'
-  integer, intent(IN) :: sz,comm
-  integer, intent(OUT) :: ierr
-  type(rpncomm_window), intent(OUT) :: mywin
-  integer(KIND=MPI_ADDRESS_KIND) :: sz8
+!InTf!  import :: rpncomm_window                           !InTf!
+  integer, intent(IN) :: sz,comm                           !InTf!
+  integer, intent(OUT) :: ierr                             !InTf!
+  type(rpncomm_window), intent(OUT) :: mywin               !InTf!
+  integer(KIND=MPI_ADDRESS_KIND) :: sz8, address
 
   mywin%p  = C_NULL_PTR    ! precondition to error condition
   mywin%t1 = 0
@@ -41,55 +42,68 @@ subroutine RPN_COMM_ezwin_create(sz,comm,mywin,ierr)
   ent = ent + 1    ! bump table pointer
 
   sz8 = sz
-  call MPI_WIN_ALLOCATE(sz8, 4, MPI_INFO_NULL, comm, ezwtab(ent)%p,ierr)
-  if(ierr .ne. MPI_SUCCESS) return    !! error creating window
+  call MPI_WIN_ALLOCATE(sz8, 4, MPI_INFO_NULL, comm, ezwtab(ent)%p,ezwtab(ent)%win,ierr)
+  if(ierr .ne. MPI_SUCCESS) then    ! error creating window
+    ent = ent - 1                   ! suppress entry in table
+    return
+  endif
+!   print *,"INFO: created window of size",sz8," bytes"
 
   mywin%p = ezwtab(ent)%p
   mywin%t1 = ieor(ent,RPN_COMM_MAGIC2)
   mywin%t2 = ent
-end subroutine RPN_COMM_ezwin_create
+end subroutine RPN_COMM_ezwin_create                       !InTf!
 
-function RPN_COMM_ezwin_id(mywin) result(winid)
+function RPN_COMM_ezwin_id(mywin) result(winid)            !InTf!
   use rpn_comm_ezwin_mod
   implicit none
   include 'mpif.h'
-  type(rpncomm_window), intent(IN) :: mywin
-  integer :: winid
+!InTf!  import :: rpncomm_window                           !InTf!
+  type(rpncomm_window), intent(IN) :: mywin                !InTf!
+  integer :: winid                                         !InTf!
 
   winid = MPI_WIN_NULL
   if(is_invalid(mywin)) return
   winid = ezwtab(mywin%t2)%win
-end function RPN_COMM_ezwin_id
+end function RPN_COMM_ezwin_id                             !InTf!
 
-function RPN_COMM_ezwin_ptr(mywin) result(winptr)
+function RPN_COMM_ezwin_ptr(mywin) result(winptr)          !InTf!
   use rpn_comm_ezwin_mod
   implicit none
   include 'mpif.h'
-  type(rpncomm_window), intent(IN) :: mywin
-  type(C_PTR) :: winptr
+!InTf!  import :: rpncomm_window,    C_PTR                 !InTf!
+  type(rpncomm_window), intent(IN) :: mywin                !InTf!
+  type(C_PTR) :: winptr                                    !InTf!
 
-  winptr = ezwtab(mywin%t2)%p
-end function RPN_COMM_ezwin_ptr
+  if(is_invalid(mywin))then
+    winptr = C_NULL_PTR
+  else
+    winptr = ezwtab(mywin%t2)%p
+  endif
+end function RPN_COMM_ezwin_ptr                            !InTf!
 
-function RPN_COMM_ezwin_size(mywin) result(sz)
+function RPN_COMM_ezwin_size(mywin) result(sz)             !InTf!
   use rpn_comm_ezwin_mod
   implicit none
   include 'mpif.h'
-  type(rpncomm_window), intent(IN) :: mywin
-  integer :: sz
+!InTf!  import :: rpncomm_window                           !InTf!
+  type(rpncomm_window), intent(IN) :: mywin                !InTf!
+  integer :: sz                                            !InTf!
 
   sz = -1
   if(is_invalid(mywin)) return
   sz = ezwtab(mywin%t2)%sz
-end function RPN_COMM_ezwin_size
+end function RPN_COMM_ezwin_size                           !InTf!
 
-subroutine RPN_COMM_ezwin_fetch_add(mywin, add, fetch, rank, offset, ierr)
+subroutine RPN_COMM_ezwin_fetch_add(mywin, add, fetch, rank, offset, ierr)       !InTf!
   use rpn_comm_ezwin_mod
   implicit none
   include 'mpif.h'
-  type(rpncomm_window), intent(IN) :: mywin
-  integer, intent(IN) :: add, rank, offset
-  integer, intent(OUT) :: fetch, ierr
+!InTf!  import :: rpncomm_window                           !InTf!
+  type(rpncomm_window), intent(IN) :: mywin                !InTf!
+  integer, intent(IN) :: add, rank, offset                 !InTf!
+  integer, intent(OUT) :: fetch, ierr                      !InTf!
+
   integer(KIND=MPI_ADDRESS_KIND) target_disp
   integer :: assert
 
@@ -100,44 +114,174 @@ subroutine RPN_COMM_ezwin_fetch_add(mywin, add, fetch, rank, offset, ierr)
   call MPI_Win_lock(MPI_LOCK_EXCLUSIVE, rank, assert, ezwtab(mywin%t2)%win, ierr)
   call MPI_FETCH_AND_OP(add, fetch, MPI_INTEGER, rank, target_disp, MPI_SUM, ezwtab(mywin%t2)%win, ierr)
   call MPI_Win_unlock(rank, ezwtab(mywin%t2)%win, ierr)
-end subroutine RPN_COMM_ezwin_fetch_add
+end subroutine RPN_COMM_ezwin_fetch_add                    !InTf!
 
-subroutine RPN_COMM_ezwin_get(mywin,z,nw,rank,offset,ierr)
+subroutine RPN_COMM_ezwin_get(mywin,pz,nw,rank,offset,ierr)       !InTf!
   use rpn_comm_ezwin_mod
   implicit none
   include 'mpif.h'
-  type(rpncomm_window), intent(IN) :: mywin
-  integer, dimension(*), intent(OUT) :: z
-  integer, intent(IN) :: nw, rank, offset
-  integer, intent(OUT) :: ierr
+!InTf!  import :: rpncomm_window,    C_PTR                 !InTf!
+  type(rpncomm_window), intent(IN) :: mywin                !InTf!
+  type(C_PTR), intent(IN), value :: pz                     !InTf!
+  integer, intent(IN) :: nw, rank, offset                  !InTf!
+  integer, intent(OUT) :: ierr                             !InTf!
+
   integer(KIND=MPI_ADDRESS_KIND) target_disp
   integer :: assert
+  integer, dimension(:), pointer :: z
 
   ierr = ieor(MPI_SUCCESS,RPN_COMM_MAGIC2)   ! fudge an error code
   if(is_invalid(mywin)) return
   target_disp = offset
   assert = 0
+  call c_f_pointer(pz,z,[nw])
   call MPI_Win_lock(MPI_LOCK_SHARED, rank, assert, ezwtab(mywin%t2)%win, ierr)
   call MPI_get(z, nw, MPI_INTEGER, rank, target_disp, nw, MPI_INTEGER, ezwtab(mywin%t2)%win, ierr)
   call MPI_Win_unlock(rank, ezwtab(mywin%t2)%win, ierr)
-end subroutine RPN_COMM_ezwin_get
+end subroutine RPN_COMM_ezwin_get                          !InTf!
 
-subroutine RPN_COMM_ezwin_put(mywin,z,nw,rank,offset,ierr)
+subroutine RPN_COMM_ezwin_put(mywin,pz,nw,rank,offset,ierr)      !InTf!
   use rpn_comm_ezwin_mod
   implicit none
   include 'mpif.h'
-  type(rpncomm_window), intent(IN) :: mywin
-  integer, dimension(*), intent(IN) :: z
-  integer, intent(IN) :: nw, rank, offset
-  integer, intent(OUT) :: ierr
+!InTf!  import :: rpncomm_window,    C_PTR                 !InTf!
+  type(rpncomm_window), intent(IN) :: mywin                !InTf!
+  type(C_PTR), intent(IN), value :: pz                     !InTf!
+  integer, intent(IN) :: nw, rank, offset                  !InTf!
+  integer, intent(OUT) :: ierr                             !InTf!
+
   integer(KIND=MPI_ADDRESS_KIND) target_disp
   integer :: assert
+  integer, dimension(:), pointer :: z
 
   ierr = ieor(MPI_SUCCESS,RPN_COMM_MAGIC2)   ! fudge an error code
   if(is_invalid(mywin)) return
   target_disp = offset
   assert = 0
+  call c_f_pointer(pz,z,[nw])
+! print 101,"PUT",z(1:nw)," to",rank,target_disp
+! 101 format(A,4I6,A,2I6)
   call MPI_Win_lock(MPI_LOCK_SHARED, rank, assert, ezwtab(mywin%t2)%win, ierr)
-  call MPI_get(z, nw, MPI_INTEGER, rank, target_disp, nw, MPI_INTEGER, ezwtab(mywin%t2)%win, ierr)
+  call MPI_put(z, nw, MPI_INTEGER, rank, target_disp, nw, MPI_INTEGER, ezwtab(mywin%t2)%win, ierr)
   call MPI_Win_unlock(rank, ezwtab(mywin%t2)%win, ierr)
-end subroutine RPN_COMM_ezwin_put
+end subroutine RPN_COMM_ezwin_put                          !InTf!
+
+#if defined(SELF_TEST)
+program test_ezwin
+  use ISO_C_BINDING
+  include 'RPN_COMM.inc'
+  include 'mpif.h'
+
+interface
+  include 'new.inc'
+end interface
+
+  integer :: comm0, error, szreq, szans, myrank, mysize, i, j, k
+  type(rpncomm_window) :: req_win, ans_win
+  integer :: me_x, me_y, fetch, partner
+  integer, dimension(-1:4,-1:4) :: petab
+  type(C_PTR) :: ptrreq, ptrans
+  integer, dimension(:), pointer :: freq, fans
+  integer, dimension(10), target :: reqbuf, ansbuf
+  integer :: ansoffset
+  integer, dimension(100) :: expected
+  real, dimension(100) :: expectedf
+  integer, dimension(2,50) :: req
+  real :: tmp
+  type :: packet
+    SEQUENCE
+    integer :: n
+    integer :: me
+    integer :: off
+    real :: val
+  end type
+  type(packet) :: pak
+  type :: area
+    SEQUENCE
+    integer :: offset
+    type(packet), dimension(10) :: pak
+  end type
+  type(area), pointer :: pakp
+
+  call mpi_init(error)
+  comm0 = MPI_COMM_WORLD
+  call mpi_comm_rank(comm0,myrank,error)
+  call mpi_comm_size(comm0,mysize,error)
+  petab = -1
+  me_y = myrank/4
+  me_x = mod(myrank,4)
+  k = 0
+  do j = 0,3
+  do i = 0,3
+    petab(i,j)=k
+    k = k + 1
+  enddo
+  enddo
+  if(me_x == 0 .and. me_y == 0)then
+    do j = 3,0,-1
+      print 100,petab(0:3,j)
+    enddo
+  endif
+100 format(40I4)
+
+  szreq = 1024*1024*3
+  szans = 1024*1024
+  call RPN_COMM_ezwin_create(szreq,comm0,req_win,error)
+  ptrreq = RPN_COMM_ezwin_ptr(req_win)
+  call c_f_pointer(ptrreq,freq,[szreq/4])
+  freq(1:szreq/4) = 0
+  freq(1) = 1
+  call c_f_pointer(ptrreq,pakp)
+
+  call RPN_COMM_ezwin_create(szans,comm0,ans_win,error)
+  ptrans = RPN_COMM_ezwin_ptr(ans_win)
+  call c_f_pointer(ptrans,fans,[szans/4])
+  fans(1:szans/4) = 0
+  expected = -1
+
+  ansoffset = 0
+  do j = -1,1
+  do i = -1,1
+    if(i .ne.0 .or. j .ne. 0) then
+      partner = petab(i+me_x,j+me_y)
+      if(partner .ne. -1) then
+        call RPN_COMM_ezwin_fetch_add(req_win, 4, fetch, partner, 0, error)
+!         print *,"request to",partner," offset =",fetch
+        req(1,1+ansoffset) = partner
+        req(2,1+ansoffset) = fetch
+        reqbuf(1:4) = [1,myrank,ansoffset,100*myrank]
+        pak = packet(1,myrank,ansoffset,1.0*myrank)
+        expected(1+ansoffset) = reqbuf(4) + partner ! expected answer from partner is element 4 of request + partner rank
+        expectedf(1+ansoffset) = pak%val + .01*partner
+!         call RPN_COMM_ezwin_put(req_win,c_loc(reqbuf(1)),4,partner,fetch,error)
+        call RPN_COMM_ezwin_put(req_win,c_loc(pak%n),4,partner,fetch,error)
+        ansoffset = ansoffset + 1
+      endif
+    endif
+  enddo
+  enddo
+  call mpi_barrier(comm0,ierr)
+  print 103,"target/offset pairs: ",req(:,1:ansoffset)
+103 format(A,20(2I3,3X))
+  print *,"total requests size =",freq(1)-1
+!   print 101,freq(2:freq(1))
+  print 105,pakp%pak(1:(freq(1)-1)/4)
+101 format(10(4I5,3x))
+105 format(10(3I5,F6.2,3x))
+
+  do i = 2,freq(1)-1,4  ! answer to requests
+    ansbuf(1) = myrank + freq(i+3)    ! answer = me + element 4 of request
+    tmp = myrank*.01 + transfer(freq(i+3),tmp)
+!     call RPN_COMM_ezwin_put(ans_win,c_loc(ansbuf(1)),1,freq(i+1),freq(i+2),error)
+    call RPN_COMM_ezwin_put(ans_win,c_loc(tmp),1,freq(i+1),freq(i+2),error)
+  enddo
+  call mpi_barrier(comm0,ierr)
+!   print 102,"expected",expected(1:ansoffset)
+  print 104,"expected",transfer(expectedf(1:ansoffset),tmp,ansoffset)
+  print 104,"answers ",transfer(fans(1:ansoffset),tmp,ansoffset)
+102 format(A,40I6)
+104 format(A,40F6.2)
+  call mpi_finalize(error)
+  stop
+end program
+#endif
